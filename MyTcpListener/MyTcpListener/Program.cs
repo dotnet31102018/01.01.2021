291f6d6451e85edc6c90bada648af7c83ca0e0dc
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2021-01-01 18:34:59 +0200

    Add files via upload

---
+++ MyTcpListener/MyTcpListener/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTcpListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client ...");
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 8010);
+            
+            Console.WriteLine("Connected to server at 127.0.0.1:8010");
+            Console.WriteLine("What do you want to send?");
+
+            string message = Console.ReadLine();
+
+            ASCIIEncoding enc = new ASCIIEncoding();
+            byte[] b = enc.GetBytes(message);
+
+            Console.WriteLine("Firing ...");
+            Stream stm = client.GetStream();
+            stm.Write(b, 0, b.Length);
+
+            // server responded with ack
+            byte[] resp = new byte[100];
+            int size = stm.Read(resp, 0, 100);
+            for (int i = 0; i < resp.Length; i++)
+            {
+                Console.Write(Convert.ToChar(resp[i])); // 'A' 65 'B' 66
+            }
+            client.Close();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MyTcpServer/MyTcpServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                IPAddress ipad = IPAddress.Parse("127.0.0.1");
+                TcpListener listener = new TcpListener(ipad, 8010);
+                listener.Start();
+
+                Console.WriteLine("The server is running at port 8010");
+                Console.WriteLine("Waiting for connection...");
+                
+                Socket s = listener.AcceptSocket(); // blocking
+
+                // when message arrives
+                Console.WriteLine($"Connection accepted from {s.RemoteEndPoint}");
+                byte[] b = new byte[1000];
+                int size = s.Receive(b);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(Convert.ToChar(b[i]));
+                    sb.Append(Convert.ToChar(b[i]).ToString());
+                }
+
+                ASCIIEncoding enc = new ASCIIEncoding();
+                s.Send(enc.GetBytes($"The string {sb} was recieved by the server."));
+
+                s.Close();
+                listener.Stop();
+                Console.WriteLine();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
